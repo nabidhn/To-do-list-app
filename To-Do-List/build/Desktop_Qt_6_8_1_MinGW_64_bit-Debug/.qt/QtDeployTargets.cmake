2c3a9b8644f97821f3ada967c4563bc13adda70d
Nabid Hasan <nabid9hasan@gmail.com>
2025-06-15 16:40:15 +0300

    exe

---
+++ To-Do-List/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_To-Do-List_FILE C:/Users/nabid/Documents/To-Do-List/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/To-Do-List.exe)
+set(__QT_DEPLOY_TARGET_To-Do-List_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_To-Do-List_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
